3abf86146b9deba0161e2244ea7cfd1fac5daa8a
Chris Sells <csells@sellsbrothers.com>
2017-02-05 16:59:59 -0800

    initial commit
    a quick hack to query an HTML file interactively with a CSS selector
    query

---
+++ SelectorDesigner/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication1 {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.label1 = new System.Windows.Forms.Label();
+      this.urlTextBox = new System.Windows.Forms.TextBox();
+      this.contentTextBox = new System.Windows.Forms.TextBox();
+      this.selectorTextBox = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(26, 40);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(54, 25);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "URL";
+      // 
+      // urlTextBox
+      // 
+      this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.urlTextBox.Location = new System.Drawing.Point(143, 40);
+      this.urlTextBox.Name = "urlTextBox";
+      this.urlTextBox.Size = new System.Drawing.Size(900, 31);
+      this.urlTextBox.TabIndex = 1;
+      this.urlTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.urlTextBox_KeyDown);
+      // 
+      // contentTextBox
+      // 
+      this.contentTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.contentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.contentTextBox.Location = new System.Drawing.Point(12, 149);
+      this.contentTextBox.Multiline = true;
+      this.contentTextBox.Name = "contentTextBox";
+      this.contentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.contentTextBox.Size = new System.Drawing.Size(1031, 616);
+      this.contentTextBox.TabIndex = 4;
+      // 
+      // selectorTextBox
+      // 
+      this.selectorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.selectorTextBox.Location = new System.Drawing.Point(143, 86);
+      this.selectorTextBox.Name = "selectorTextBox";
+      this.selectorTextBox.Size = new System.Drawing.Size(900, 31);
+      this.selectorTextBox.TabIndex = 3;
+      this.selectorTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.selectorTextBox_KeyDown);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(26, 86);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(91, 25);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Selector";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1055, 777);
+      this.Controls.Add(this.selectorTextBox);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.contentTextBox);
+      this.Controls.Add(this.urlTextBox);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox urlTextBox;
+    private System.Windows.Forms.TextBox contentTextBox;
+    private System.Windows.Forms.TextBox selectorTextBox;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ SelectorDesigner/Form1.cs
@@ -0,0 +1,79 @@
+﻿// from https://github.com/AngleSharp
+// with https://github.com/ericchiang/pup
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html;
+
+namespace WindowsFormsApplication1 {
+  public partial class Form1 : Form {
+    static IMarkupFormatter formatter = new PrettyMarkupFormatter { Indentation = "  ", NewLine = "\r\n" };
+    IDocument html;
+
+    public Form1() {
+      InitializeComponent();
+
+      // TODO: remove this when the shit gets real...
+      urlTextBox.Text = "https://news.ycombinator.com/";
+
+      if (urlTextBox.Text != "") {
+        FetchHtml(urlTextBox.Text);
+      }
+    }
+
+    void urlTextBox_KeyDown(object sender, KeyEventArgs e) {
+      if (e.KeyCode == Keys.Enter) {
+
+        Uri uri;
+        if (!Uri.TryCreate(urlTextBox.Text, UriKind.Absolute, out uri)) {
+          // try to tack on "http://"
+          Uri.TryCreate("http://" + urlTextBox.Text, UriKind.Absolute, out uri);
+        }
+
+        if (uri == null) { MessageBox.Show($"invalid URL: '{urlTextBox.Text}'"); }
+        FetchHtml(uri.ToString());
+        e.Handled = e.SuppressKeyPress = true;
+      }
+    }
+
+    void selectorTextBox_KeyDown(object sender, KeyEventArgs e) {
+      if (e.KeyCode == Keys.Enter && this.html != null) {
+        QueryHtml(selectorTextBox.Text);
+        e.Handled = e.SuppressKeyPress = true;
+      }
+    }
+
+    async void FetchHtml(string url) {
+      this.html = await BrowsingContext.New(Configuration.Default.WithDefaultLoader()).OpenAsync(url);
+      QueryHtml(selectorTextBox.Text);
+    }
+
+    static string ToHtml(IElement elem) {
+      var writer = new StringWriter();
+      elem.ToHtml(writer, formatter);
+      return writer.ToString();
+    }
+
+    void QueryHtml(string selector) {
+      Debug.Assert(this.html != null);
+
+      try {
+        var sb = new StringBuilder();
+
+        foreach (var elem in html.QuerySelectorAll(selector)) {
+          sb.AppendLine(ToHtml(elem));
+        }
+        contentTextBox.Text = sb.ToString();
+      }
+      catch (Exception ex) {
+        contentTextBox.Text = ex.Message;
+      }
+
+    }
+
+  }
+}
